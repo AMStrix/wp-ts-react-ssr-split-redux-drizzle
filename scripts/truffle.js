335a9993134cfb6e7782643c655849620406549d
Aaron <aaron.m.stricker@gmail.com>
2018-09-06 11:00:29 -0500

    - add fundWeb3v1 fn for running from dev-server - make ganache-cli deterministic (so account[0] stays same)

--- scripts/truffle.js
+++ scripts/truffle.js
@@ -6,20 +6,21 @@ const Web3 = require('web3');
 
 const paths = require('../config/paths');
 const truffleConfig = require('../truffle');
-const { logMessage, isPortTaken } = require('./utils');
+const { logMessage } = require('./utils');
 
 require('../config/env');
 
 module.exports = {};
 
-module.exports.fund = web3 => {
+const fund = (module.exports.fund = web3 => {
   // Fund ETH accounts
   const ethAccounts = process.env.FUND_ETH_ADDRESSES
     ? process.env.FUND_ETH_ADDRESSES.split(',').map(a => a.trim())
     : [];
-
+  console.log(web3);
+  console.log('accounts[0]', web3.eth.accounts[0]);
   if (ethAccounts.length) {
-    console.info('Sending 50 ETH to the following addresses...');
+    logMessage('Sending 50 ETH to the following addresses...', 'info');
     ethAccounts.forEach((addr, i) => {
       try {
         web3.eth.sendTransaction({
@@ -27,15 +28,19 @@ module.exports.fund = web3 => {
           from: web3.eth.accounts[i],
           value: web3.toWei('50', 'ether'),
         });
-        console.info(`    ${addr} <- from ${web3.eth.accounts[i]}`);
+        logMessage(`    ${addr} <- from ${web3.eth.accounts[i]}`);
       } catch (error) {
-        console.log(`    Error sending funds to ${addr} : ${error}`);
+        logMessage(`    Error sending funds to ${addr} : ${error}`, 'error');
       }
     });
   } else {
-    console.info('No accounts specified for funding in .env file...');
+    logMessage('No accounts specified for funding in .env file...', 'warning');
   }
-};
+});
+
+const clean = (module.exports.clean = () => {
+  rimraf.sync(paths.contractsBuild);
+});
 
 const compile = (module.exports.compile = () => {
   childProcess.execSync('truffle compile');
@@ -105,6 +110,41 @@ const checkContractsNetworkIds = (module.exports.checkContractsNetworkIds = id =
     });
   }));
 
+const fundWeb3v1 = (module.exports.fundWeb3v1 = () => {
+  // Fund ETH accounts
+  const ethAccounts = process.env.FUND_ETH_ADDRESSES
+    ? process.env.FUND_ETH_ADDRESSES.split(',').map(a => a.trim())
+    : [];
+  const web3 = createWeb3();
+  web3.eth.getAccounts().then(accts => {
+    if (ethAccounts.length) {
+      logMessage('Sending 50% of ETH balance from accounts...', 'info');
+      const txs = ethAccounts.map((addr, i) => {
+        return web3.eth
+          .getBalance(accts[i])
+          .then(parseInt)
+          .then(bal => {
+            const amount = '' + Math.round(bal / 2);
+            const amountEth = web3.utils.fromWei(amount);
+            return web3.eth
+              .sendTransaction({
+                to: addr,
+                from: accts[i],
+                value: amount,
+              })
+              .then(() => logMessage(`    ${addr} <- ${amountEth} from ${accts[i]}`))
+              .catch(e =>
+                logMessage(`    Error sending funds to ${addr} : ${e}`, 'error'),
+              );
+          });
+      });
+      Promise.all(txs).then(() => web3.currentProvider.connection.close());
+    } else {
+      logMessage('No accounts specified for funding in .env file...', 'warning');
+    }
+  });
+});
+
 module.exports.ethereumCheck = () =>
   isGanacheUp(true)
     .then(isUp => !isUp && Promise.reject('network down'))
@@ -113,6 +153,7 @@ module.exports.ethereumCheck = () =>
     .then(allHaveId => {
       if (!allHaveId) {
         logMessage('Contract problems, will compile & migrate.', 'warning');
+        clean();
         logMessage('truffle compile, please wait...', 'info');
         compile();
         logMessage('truffle migrate, please wait...', 'info');
@@ -121,4 +162,5 @@ module.exports.ethereumCheck = () =>
         logMessage('OK, Contracts have correct network id.', 'info');
       }
     })
+    .then(() => fundWeb3v1())
     .catch(e => logMessage('WARNING: ethereum setup has a problem: ' + e, 'error'));
